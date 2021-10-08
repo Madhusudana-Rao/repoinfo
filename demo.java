f0ea95fc05540c1d34c92b58bcf634de00f9306b
Madhusudana Rao <53528070+Madhusudana-Rao@users.noreply.github.com>
2021-10-08 14:01:12 +0530

    Create demo.java

---
+++ demo.java
@@ -0,0 +1,8 @@
+public class demo{
+  public static void main(String[] args){
+  System.out.println("Hello World");
+  System.out.println("This is Github");
+  System.out.println("repoinfo Repository");
+  System.out.println("Good Luck");
+}
+}
