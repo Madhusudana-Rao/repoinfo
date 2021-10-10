27b2258719bd2445eb54bd707bf3e44d0ec46266
Madhusudana Rao <madhu.rao@zohocorp.com>
2021-10-10 20:41:24 +0530

    Revert "Revert "Commit from Dev2(1)""
    This reverts commit c089bae63bb8d59b3a1af4730e76671b3ea72705.

--- demo.java
+++ demo.java
@@ -6,8 +6,5 @@ public class demo{
   System.out.println("This is revised source code, Dev2");
   System.out.println("I,ve reviwed the code and fixed the errors");
   System.out.println("Thank you. Have a nice day");
-  System.out.println("repoinfo Repository from Dev2");
-  System.out.println("I'm changing this");
-  System.out.println("Dev1, please fix if any errors");
 }
 }
