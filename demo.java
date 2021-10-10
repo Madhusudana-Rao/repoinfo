55dcd51351a40f9e7ac5016d7d7defdfe3875d76
Madhusudana Rao <madhu.rao@zohocorp.com>
2021-10-10 20:01:28 +0530

    Commit from Dev1(1)

--- demo.java
+++ demo.java
@@ -1,9 +1,9 @@
 public class demo{
   public static void main(String[] args){
-  System.out.println("Good Evening from Dev2");
+  System.out.println("Good Evening from Dev1");
   System.out.println("This is Github");
-  System.out.println("repoinfo Repository from Dev2");
-  System.out.println("I'm changing this");
-  System.out.println("Dev1, please fix if any errors");
+  System.out.println("repoinfo Repository from Dev1");
+  System.out.println("I'm changing this source code (Dev1)");
+  System.out.println("Dev2, I've Fixed the errors. Thank You");
 }
 }
